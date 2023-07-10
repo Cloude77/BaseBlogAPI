33a8d5244fd27659def4fd75bdacd4762da76cb7
Your name <youemail@inbox.ru>
2023-07-10 20:18:20 +0300

    Create models, urls, views, serializers.

--- posts/tests.py
+++ posts/tests.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from .models import Post
 
 
-class BlogTest(TestCase):
+class BlogTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
@@ -12,8 +12,17 @@ class BlogTest(TestCase):
             username='testuser1', password='abc123')
         testuser1.save()
 
-    # Create a blog post
+        # Create a blog post
 
         test_post = Post.objects.create(
+            author=testuser1, title='Blog title', body='Body content...')
+        test_post.save()
 
-        )
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        expected_author = f'{post.author}'
+        expected_tittle = f'{post.title}'
+        expected_body = f'{post.body}'
+        self.assertEqual(expected_author, 'testuser1')
+        self.assertEqual(expected_tittle, 'Blog title')
+        self.assertEqual(expected_body, 'Body content...')
